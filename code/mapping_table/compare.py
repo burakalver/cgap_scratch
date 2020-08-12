5ceb69a17b2554655cc1b0b20dab2ffbd2acfe3d
Burak Alver <burak_alver@hms.harvard.edu>
2020-08-12 18:38:09 -0400

    mapping table updates

--- code/mapping_table/compare.py
+++ code/mapping_table/compare.py
@@ -7,29 +7,47 @@ from os import path
 
 import pandas as pd
 
+import base
 DATA_DIR = path.join(base.ROOT_DIR, "data")
 
 def compare_df(df1, df2):
     '''
     given two pandas dataframes, print out difference in row indexes, and values.
     '''
-    intersection = [field for field in df1.index if field in df2.index]
+
+    intersection_rows = [field for field in df1.index if field in df2.index]
     for field in df1.index:
-        if not field in intersection:
+        if not field in intersection_rows:
             print("Only in df1: ", field)
     for field in df2.index:
-        if not field in intersection:
+        if not field in intersection_rows:
             print("Only in df2: ", field)
 
-    for field in intersection:
+    intersection_cols = [field for field in df1.columns if field in df2.columns]
+    for field in df1.columns:
+        if not field in intersection_cols:
+            print("Only in df1: ", field)
+    for field in df2.columns:
+        if not field in intersection_cols:
+            print("Only in df2: ", field)
+
+    df1 = df1[intersection_cols]
+    df2 = df2[intersection_cols]
+
+    for field in intersection_rows:
         comp = (df1.loc[field] == df2.loc[field])
         if not all(comp):
             mismatch_cols = list(comp[comp == 0].index)
             print("%s:\t%s" % (field, ", ".join(mismatch_cols)))
 
+
+            
 if __name__ == '__main__':
-    fname_old = path.join(DATA_DIR, 'VCF Mapping Table - v0.4 variant table - stable.tsv')
-    fname_new = path.join(DATA_DIR, 'VCF Mapping Table - v0.4.7 variant table - stable.tsv')
+#    fname_old = path.join(DATA_DIR, 'VCF Mapping Table - v0.4.7 variant table - stable.tsv')
+#    fname_new = path.join(DATA_DIR, 'VCF Mapping Table - v0.4.8 variant table.tsv')
+    fname_old = path.join(DATA_DIR, 'VCF Mapping Table - GeneTable v0.4.5 - stable.tsv')
+    fname_new = path.join(DATA_DIR, 'VCF Mapping Table - GeneTable v0.4.6.tsv')
+
     KEY_FIELD = "field_name"
 
     df_old = pd.read_csv(fname_old, sep='\t', header=5, na_filter=False)
