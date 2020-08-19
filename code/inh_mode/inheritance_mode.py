77ed78aaa91b72d997bea6c40a6b69414944e0e0
Burak Alver <burak_alver@hms.harvard.edu>
2020-08-19 11:23:33 -0400

    fix .. for female chry, xlined dominant; add loh and nones to inh_modes

--- code/inh_mode/inheritance_mode.py
+++ code/inh_mode/inheritance_mode.py
@@ -26,6 +26,27 @@ GENOTYPE_LABEL_M = "hemi alt"
 GENOTYPE_LABEL_FEMALE_CHRY = "-"
 GENOTYPE_LABEL_SEX_INCONSISTENT = "false"
 
+INHMODE_LABEL_DE_NOVO_STRONG = "de novo (strong)"
+INHMODE_LABEL_DE_NOVO_MEDIUM = "de novo (medium)"
+INHMODE_LABEL_DE_NOVO_WEAK = "de novo (weak)"
+INHMODE_LABEL_DE_NOVO_CHRXY = "de novo (chrXY)"
+INHMODE_DOMINANT_FATHER = "dominant (paternal)"
+INHMODE_DOMINANT_MOTHER = "dominant (maternal)"
+INHMODE_LABEL_RECESSIVE = "recessive"
+INHMODE_LABEL_X_LINKED_RECESSIVE_MOTHER = "X-linked recessive (Maternal)"
+INHMODE_LABEL_X_LINKED_DOMINANT_MOTHER = "X-linked dominant (Maternal)"
+INHMODE_LABEL_X_LINKED_DOMINANT_FATHER = "X-linked dominant (Paternal)"
+INHMODE_LABEL_Y_LINKED = "Y-linked dominant"
+INHMODE_LABEL_LOH = "Loss of Heteozyogousity"
+
+INHMODE_LABEL_NONE_DOT = "Low relevance, missing call(s) in family"
+INHMODE_LABEL_NONE_MN = "Low relevance, multiallelic site family"
+INHMODE_LABEL_NONE_SEX_INCONSISTENT = "Low relevance, mismatching chrXY genotype(s)"
+INHMODE_LABEL_NONE_LOWDEPTH = "Low relevance, low depth"
+INHMODE_LABEL_NONE_HOMOZYGOUS_PARENT = "Low relevance, homozygous in a parent"
+INHMODE_LABEL_NONE_BOTH_PARENTS = "Low relevance, present in both parent(s)"
+INHMODE_LABEL_NONE_OTHER = "Low relevance, other"
+
 def multiallelic_site(genotype):
     '''
     boolean for "any genotype has allele number 2 or greater"
@@ -49,6 +70,8 @@ def genotype_to_genotype_label_single(igenotype, isex, chrom):
     genotype_allele1, genotype_allele2 = igenotype.split("/")
 
     if genotype_allele1 == ".":
+        if isex == "female" and chrom == "chrY":
+            return GENOTYPE_LABEL_FEMALE_CHRY
         return GENOTYPE_LABEL_DOT
 
     genotype_allele1 = int(genotype_allele1)
@@ -111,39 +134,39 @@ def inheritance_modes_trio(genotype, genotype_label, sex, chrom, novoPP):
     if "father" not in roles or "mother" not in roles or "self" not in roles:
         return []
 
+    if GENOTYPE_LABEL_DOT in genotype_label.values():
+        return []
+    if multiallelic_site(genotype):
+        return []
     if GENOTYPE_LABEL_SEX_INCONSISTENT in genotype_label.values():
         return []
 
-    if multiallelic_site(genotype):
-        return []
-
     if novoPP > 0.9:
-        return ["de novo (strong)"]
+        return [INHMODE_LABEL_DE_NOVO_STRONG]
     if novoPP > 0.1:
-        return ["de novo (medium)"]
+        return [INHMODE_LABEL_DE_NOVO_MEDIUM]
 
     if (genotype["mother"] == "0/0" and genotype["father"] == "0/0"
             and genotype["self"] == "0/1" and chrom == "autosome"):
-        return ["de novo (weak)"]
+        return [INHMODE_LABEL_DE_NOVO_WEAK]
 
     if (genotype["mother"] == "0/0" and genotype["father"] == "0/0"
             and ((genotype["self"] == "0/1" and sex["self"] == "female" and chrom == "chrX")
                  or (genotype["self"] == "1/1" and sex["self"] == "male" and chrom != "autosome"))):
         if novoPP == 0:
-            return ["de novo (weak)"]
+            return [INHMODE_LABEL_DE_NOVO_WEAK]
         if novoPP == -1:
-            return ["de novo (chrXY)"]
+            return [INHMODE_LABEL_DE_NOVO_CHRXY]
         raise ValueError("novoPP is different from 0 or -1 on sex chromosome: " + str(novoPP))
 
     if (genotype["mother"] == "0/0"
-            and (genotype_label["father"] == GENOTYPE_LABEL_0M or
-                 genotype_label["father"] == GENOTYPE_LABEL_M)
+            and genotype_label["father"] == GENOTYPE_LABEL_0M
             and genotype["self"] == "0/1"):
-        return ["dominant (paternal)"]
+        return [INHMODE_DOMINANT_FATHER]
 
     if (genotype["mother"] == "0/1" and genotype["father"] == "0/0"
             and genotype["self"] == "0/1"):
-        return ["dominant (maternal)"]
+        return [INHMODE_DOMINANT_MOTHER]
 
     if (genotype["mother"] == "0/1" and genotype["father"] == "0/1"
             and genotype["self"] == "1/1"):
@@ -151,17 +174,50 @@ def inheritance_modes_trio(genotype, genotype_label, sex, chrom, novoPP):
 
     if (genotype["mother"] == "0/1" and genotype["father"] == "0/0"
             and genotype["self"] == "1/1" and sex["self"] == "male" and chrom == "chrX"):
-        return ["X-linked recessive (Maternal)", "X-linked dominant (Maternal)"]
+        return [INHMODE_LABEL_X_LINKED_RECESSIVE_MOTHER, INHMODE_LABEL_X_LINKED_DOMINANT_MOTHER]
 
-    if (genotype["mother"] == "0/0" and genotype["father"] == "1/1"
-            and genotype["self"] == "1/1" and sex["self"] == "male" and chrom == "chrX"):
-        return ["X-linked dominant (Paternal)"]
+    if (genotype["mother"] == "0/0" and genotype_label["father"] == GENOTYPE_LABEL_M and
+            chrom == "chrX" and genotype_label["self"] in [GENOTYPE_LABEL_M, GENOTYPE_LABEL_0M]):
+        return [INHMODE_LABEL_X_LINKED_DOMINANT_FATHER]
 
-    if (genotype["mother"] == "0/0" and genotype["father"] == "1/1"
-            and genotype["self"] == "1/1" and sex["self"] == "male" and chrom == "chrY"):
-        return ["Y-linked dominant"]
+    if (genotype_label["father"] == GENOTYPE_LABEL_M and
+            chrom == "chrY" and genotype_label["self"] == GENOTYPE_LABEL_M):
+        return [INHMODE_LABEL_Y_LINKED]
+
+    if (((genotype["mother"] == "0/1" and genotype["father"] == "0/0") or
+         (genotype["mother"] == "0/0" and genotype["father"] == "0/1"))
+            and genotype["self"] == "1/1"):
+        return [INHMODE_LABEL_LOH]
 
     return []
+
+
+def inheritance_modes_other_labels(genotype, genotype_label):
+    '''
+    we could leave inheritance mode field empty if none of the above fit.
+    But maybe let's add an explainer value for why it is empty.
+    '''
+    roles = genotype.keys()
+
+    #proband-only or duos can be added later
+    if "father" not in roles or "mother" not in roles or "self" not in roles:
+        return []
+
+    if GENOTYPE_LABEL_DOT in genotype_label.values():
+        return [INHMODE_LABEL_NONE_DOT]
+    if multiallelic_site(genotype):
+        return [INHMODE_LABEL_NONE_MN]
+    if GENOTYPE_LABEL_SEX_INCONSISTENT in genotype_label.values():
+        return [INHMODE_LABEL_NONE_SEX_INCONSISTENT]
+
+    if genotype["mother"] == "1/1" or (
+            genotype["father"] == "1/1" and genotype_label["father"] != GENOTYPE_LABEL_M):
+        return [INHMODE_LABEL_NONE_HOMOZYGOUS_PARENT]
+    if ((genotype["mother"] == "1/1" or genotype["mother"] == "0/1") and
+            (genotype["father"] == "1/1" or genotype["father"] == "0/1")):
+        return [INHMODE_LABEL_NONE_BOTH_PARENTS]
+
+    return [INHMODE_LABEL_NONE_OTHER]
 
 
 def inheritance_modes_cmphet(cmphet):
@@ -233,6 +289,8 @@ def inheritance_mode(variant):
     genotype_label = genotype_to_genotype_label_family(genotype, sex, chrom)
     inheritance_modes = inheritance_modes_trio(genotype, genotype_label, sex, chrom, novoPP)
     inheritance_modes += inheritance_modes_cmphet(cmphet)
+    if len(inheritance_modes) == 0:
+        inheritance_modes = inheritance_modes_other_labels(genotype, genotype_label)
 
     result = {
         "genotype_label": genotype_label,
